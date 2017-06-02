f91ebdc99cf8db2343ef41ff5bb276d08df75ada
luke pate <lukewpate@gmail.com>
2017-06-02 10:36:35 -0500

    add state to time

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
 import './App.css';
 
 class App extends Component {
@@ -10,7 +11,7 @@ class App extends Component {
     }
   }
   changeDeadline() {
-    this.setState({deadline: 'November 25, 2017'})
+    this.setState({deadline: this.state.newDeadline})
   }
 
   render() {
@@ -18,13 +19,12 @@ class App extends Component {
       <div className="App">
         <div className="App-title">Countdown to {this.state.deadline}</div>
         <div>
-          <div className="Clock-days">14 days</div>
-          <div className="Clock-hours">30 hours</div>
-          <div className="Clock-minutes">15 minutes</div>
-          <div className="Clock-seconds">20 seconds</div>
+          <Clock />
         </div>
         <div>
-          <input placeholder="new date"/>
+          <input placeholder="new date"
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
           <button onClick={() => this.changeDeadline()}>submit</button>
         </div>
 
